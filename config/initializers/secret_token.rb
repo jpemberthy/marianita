5536ddd47f67ac50f37faddf73bbf700a6e4280d
Juan Pemberthy <jpemberthy@gmail.com>
2012-09-05 21:09:21 -0500

    rico

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marianita::Application.config.secret_token = '22ea976b9fc9e49242540db1e3b2464075ea209167fe83f5f3c9df694ef8a01ca528e9d852f1b211617688993a554d810f1ff3fbb02fe6719b6894ff4dc0c9ba'
