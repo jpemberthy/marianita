74391775a1a18d5b3927e610004f1c75af43fca0
Juan Pemberthy <jpemberthy@gmail.com>
2012-09-13 17:28:49 -0500

    Added update support to FacebookFeed.create_from_facebook.

--- app/models/facebook_feed.rb
+++ app/models/facebook_feed.rb
@@ -1,14 +1,26 @@
 class FacebookFeed
   include Mongoid::Document
   field :user_id, type: Integer
+  field :facebook_id, type: String
 
-  # TODO: validations!
+  validates_presence_of :user_id, :facebook_id
+  validates_uniqueness_of :facebook_id
 
-  # TODO: make it work with update. (find by facebook_id)
   def self.create_from_facebook(user_id, facebook_feed)
-    facebook_feed.merge!(user_id: user_id, facebook_id: facebook_feed["id"])
+    facebook_id = facebook_feed["id"]
+
+    if feed = where(facebook_id: facebook_id).first
+      feed.update_from_facebook(facebook_feed)
+    else
+      facebook_feed.merge!(user_id: user_id, facebook_id: facebook_id)
+      facebook_feed.delete("id")
+      create(facebook_feed)
+    end
+  end
+
+  def update_from_facebook(facebook_feed)
     facebook_feed.delete("id")
-    create(facebook_feed)
+    update_attributes(facebook_feed)
   end
 
 end
