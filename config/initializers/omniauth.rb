257d14fd9522b11ac47e13203dba87472831d84e
Juan Pemberthy <jpemberthy@gmail.com>
2012-09-13 17:52:13 -0500

    facebook.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,9 @@ OmniAuth.config.logger = Rails.logger
 
 # TODO: make credentials work based on the environment with fb-credentials.yml
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '268529246596995', 'a8675694ee39e3fd6ba7285611725d69',
+  credentials_path = File.join(Rails.root, %w[config facebook.yml])
+  credentials = YAML.load_file(credentials_path)[Rails.env]
+
+  provider :facebook, credentials["app_id"], credentials["secret"],
            :scope => 'email,user_location,read_stream'
 end
