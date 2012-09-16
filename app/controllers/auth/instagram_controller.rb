db2e39d90b56706f24cb2d19736ce148cfb6cc80
Juan Pemberthy <jpemberthy@gmail.com>
2012-09-16 16:28:12 -0500

    Fixing instagram stuff.

--- app/controllers/auth/instagram_controller.rb
+++ app/controllers/auth/instagram_controller.rb
@@ -1,12 +1,12 @@
 class Auth::InstagramController < ApplicationController
 
   def new
-    # TODO: fix me. Suddendly started giving me an invalid request error.
-    redirect_to Instagram.authorize_url(redirect_to: callback_url)
+    redirect_to Instagram.authorize_url(redirect_uri: callback_url)
   end
 
   def create
-    response = Instagram.get_access_token(params[:code], redirect_url: callback_url)
+    # TODO: current_user.link_instagram.
+    response = Instagram.get_access_token(params[:code], redirect_uri: callback_url)
     render json: response
   end
 
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,4 +1,4 @@
 Instagram.configure do |config|
-  config.client_id = "116925944aef40758cede9ee1886484c"
-  config.client_secret = "22a486d94b15423bb5c6d24f14f25a96"
+  config.client_id = Site.instagram_credentials["client_id"]
+  config.client_secret = Site.instagram_credentials["client_secret"]
 end
