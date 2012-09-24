cf6c7087a0e8c199b7f1f6f73b563760071eca24
Juan Pemberthy <jpemberthy@gmail.com>
2012-09-24 16:44:25 -0500

    user/instagram.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  SERVICES = ['facebook', 'twitter', 'instagram']
   has_many :tokens
   has_many :feeds
 
@@ -6,28 +7,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :twitter_id, if: Proc.new { |u| u.twitter_id.present? }
   validates_uniqueness_of :email, if: Proc.new { |u| u.email.present? }
 
-  ['facebook', 'twitter'].each do |service|
+  SERVICES.each do |service|
     include "User::#{service.classify}::InstanceMethods".constantize
     extend "User::#{service.classify}::ClassMethods".constantize
-  end
-
-  # Instagram
-  def link_instagram(instagram)
-    client = Instagram.client(:access_token => instagram.access_token)
-
-    self.tap do |user|
-      user.instagram_id = client.user.id
-      user.save!
-      user.update_instagram_token(client.access_token)
-      user
-    end
-  end
-
-  def update_instagram_token(access_token)
-    tokens.instagram.where(token: access_token).first_or_initialize.tap do |token|
-      token.provider = 'instagram'
-      token.token = access_token
-      token.save!
-    end
   end
 end
---
+++ app/models/user/instagram.rb
@@ -0,0 +1,25 @@
+module User::Instagram
+  module InstanceMethods
+    def link_instagram(instagram)
+      client = Instagram.client(:access_token => instagram.access_token)
+
+      self.tap do |user|
+        user.instagram_id = client.user.id
+        user.save!
+        user.update_instagram_token(client.access_token)
+        user
+      end
+    end
+
+    def update_instagram_token(access_token)
+      tokens.instagram.where(token: access_token).first_or_initialize.tap do |token|
+        token.provider = 'instagram'
+        token.token = access_token
+        token.save!
+      end
+    end
+  end
+
+  module ClassMethods
+  end
+end
