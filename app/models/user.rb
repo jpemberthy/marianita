6d239a8576bb98c9e878407ee08559afd10799e2
Juan Pemberthy <jpemberthy@gmail.com>
2012-09-24 16:42:32 -0500

    user/twitter module and automagically loading instance/class methods for each service.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,39 +6,9 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :twitter_id, if: Proc.new { |u| u.twitter_id.present? }
   validates_uniqueness_of :email, if: Proc.new { |u| u.email.present? }
 
-  include User::Facebook::InstanceMethods
-  extend User::Facebook::ClassMethods
-
-  # TODO: Wrap in a Twitter module.
-  def twitter
-    @twitter ||= Twitter::Client.new(twitter_token)
-  end
-
-  def twitter_token
-    t = tokens.twitter.last
-    { oauth_token: t.token, oauth_token_secret: t.oauth_token_secret }
-  end
-
-  def self.from_twitter(twitter, user=nil)
-    user ||= where(twitter_id: twitter.uid).first_or_initialize
-
-    user.tap do |user|
-      user.twitter_id = twitter.uid
-      user.name = twitter.info.name
-      user.save!
-      user.update_twitter_token(twitter.credentials)
-      user
-    end
-  end
-
-  def update_twitter_token(credentials)
-    conditions = { token: credentials.token, oauth_token_secret: credentials.secret }
-    tokens.twitter.where(conditions).first_or_initialize.tap do |token|
-      token.provider = 'twitter'
-      token.token = credentials.token
-      token.oauth_token_secret = credentials.secret
-      token.save!
-    end
+  ['facebook', 'twitter'].each do |service|
+    include "User::#{service.classify}::InstanceMethods".constantize
+    extend "User::#{service.classify}::ClassMethods".constantize
   end
 
   # Instagram
---
+++ app/models/user/twitter.rb
@@ -0,0 +1,37 @@
+module User::Twitter
+  module InstanceMethods
+    def twitter
+      @twitter ||= Twitter::Client.new(twitter_token)
+    end
+
+    def twitter_token
+      t = tokens.twitter.last
+      { oauth_token: t.token, oauth_token_secret: t.oauth_token_secret }
+    end
+
+    def update_twitter_token(credentials)
+      conditions = { token: credentials.token, oauth_token_secret: credentials.secret }
+      tokens.twitter.where(conditions).first_or_initialize.tap do |token|
+        token.provider = 'twitter'
+        token.token = credentials.token
+        token.oauth_token_secret = credentials.secret
+        token.save!
+      end
+    end
+
+  end
+
+  module ClassMethods
+    def from_twitter(twitter, user=nil)
+      user ||= where(twitter_id: twitter.uid).first_or_initialize
+
+      user.tap do |user|
+        user.twitter_id = twitter.uid
+        user.name = twitter.info.name
+        user.save!
+        user.update_twitter_token(twitter.credentials)
+        user
+      end
+    end
+  end
+end
