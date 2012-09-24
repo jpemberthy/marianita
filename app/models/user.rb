9299b6ae91d980c0f939dc4595533a129f0c7e23
Juan Pemberthy <jpemberthy@gmail.com>
2012-09-24 16:28:24 -0500

    User::Facebook Module.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,36 +6,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :twitter_id, if: Proc.new { |u| u.twitter_id.present? }
   validates_uniqueness_of :email, if: Proc.new { |u| u.email.present? }
 
-  # TODO: Wrap in a Facebook module.
-  def facebook
-    @facebook ||= Koala::Facebook::API.new(self.facebook_token)
-  end
-
-  def facebook_token
-    tokens.facebook.last.token
-  end
-
-  def self.from_fb(fb, user=nil)
-    user ||= where(facebook_id: fb.uid).first_or_initialize
-
-    user.tap do |user|
-      user.email = fb.info.email
-      user.name = fb.info.name
-      user.facebook_id = fb.uid
-      user.save!
-      user.update_fb_token(fb.credentials)
-      user
-    end
-  end
-
-  def update_fb_token(fb_credentials)
-    tokens.facebook.where(token: fb_credentials.token).first_or_initialize.tap do |token|
-      token.provider = 'facebook'
-      token.token = fb_credentials.token
-      token.expires_at = Time.at(fb_credentials.expires_at)
-      token.save!
-    end
-  end
+  include User::Facebook::InstanceMethods
+  extend User::Facebook::ClassMethods
 
   # TODO: Wrap in a Twitter module.
   def twitter
---
+++ app/models/user/facebook.rb
@@ -0,0 +1,36 @@
+module User::Facebook
+
+  module InstanceMethods
+    def facebook
+      @facebook ||= Koala::Facebook::API.new(self.facebook_token)
+    end
+
+    def facebook_token
+      tokens.facebook.last.token
+    end
+
+    def update_fb_token(fb_credentials)
+      tokens.facebook.where(token: fb_credentials.token).first_or_initialize.tap do |token|
+        token.provider = 'facebook'
+        token.token = fb_credentials.token
+        token.expires_at = Time.at(fb_credentials.expires_at)
+        token.save!
+      end
+    end
+  end
+
+  module ClassMethods
+    def from_fb(fb, user=nil)
+      user ||= where(facebook_id: fb.uid).first_or_initialize
+
+      user.tap do |user|
+        user.email = fb.info.email
+        user.name = fb.info.name
+        user.facebook_id = fb.uid
+        user.save!
+        user.update_fb_token(fb.credentials)
+        user
+      end
+    end
+  end
+end
